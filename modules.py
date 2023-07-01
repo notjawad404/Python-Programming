2ea5b49951daccfb52491af1ad16542580225919
notjawad404 <jawadali000123@gmail.com>
2023-07-01 22:47:15 +0500

    Added modules.py file

---
+++ modules.py
@@ -0,0 +1,19 @@
+import math
+import calendar
+print("Square root of 4 = ",math.sqrt(4))
+print("4 power 5 = ",math.pow(4, 5))
+
+
+print("All math functions: \n", dir(math))
+print("--------------------------------\n")
+
+
+print("Calender Module")
+month = calendar.month(2023, 7)
+print(month)
+
+print("2023 is leap year? ",calendar.isleap(2023))
+
+print("All calender functions: \n", dir(calendar))
+print("--------------------------------\n")
+
