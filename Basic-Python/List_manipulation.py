f541f780ee88a108285cceede5eeb0bb1109d27f
notjawad404 <jawadali000123@gmail.com>
2024-02-24 00:45:23 +0500

    Add List manipulation

---
+++ Basic-Python/List_manipulation.py
@@ -0,0 +1,19 @@
+list = ['Simon',6.0, "Price",5.10, "Soap",5.11]
+
+print('\n',list)
+# Add an element to the list
+list.append("Alex")
+list.append(6.1)
+
+print('\n',list)
+
+# Modify an element in the list
+list[5] = 5.10
+
+print('\n',list)
+
+# Remove an element from the list
+del list[4]
+del list[4]
+
+print('\n',list)
